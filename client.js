7d1e78d7b5de6a0990043801b29856fd4654b5ae
David Vinson <davidvinson018@gmail.com>
2024-08-27 16:15:31 -0500

    bonus calc works as expected

--- client.js
+++ client.js
@@ -45,15 +45,54 @@ const employees = [
 function calculateIndividualEmployeeBonus(employee) {
   // your logic here
   let employeeBonus = {
+    // creating employeeBonus object to later return.
     name: employee.name,
     bonusPercentage: 0,
     totalBonus: 0,
-    totalCompensation: employee.annualSalary,
+    totalCompensation: Number(employee.annualSalary),
   };
+  if (employee.reviewRating <= 2) {
+    return employeeBonus;
+  }
+  if (employee.reviewRating > 2) {
+    employeeBonus.bonusPercentage += 4 / 100;
+  }
+  if (employee.reviewRating > 3) {
+    employeeBonus.bonusPercentage += 2 / 100;
+  }
+  if (employee.reviewRating > 4) {
+    employeeBonus.bonusPercentage += 4 / 100;
+  }
+  if (employee.employeeNumber.length < 5) {
+    employeeBonus.bonusPercentage += 5 / 100;
+  }
+  if (employee.annualSalary > 65000) {
+    employeeBonus.bonusPercentage -= 1 / 100;
+  }
 
+  if (employeeBonus.bonusPercentage > 13 / 100) {
+    employeeBonus.bonusPercentage = 13 / 100;
+  } else if (employeeBonus.bonusPercentage < 0) {
+    employeeBonus.bonusPercentage = 0;
+  }
+
+  employeeBonus.totalBonus = employee.annualSalary * employeeBonus.bonusPercentage;
+  employeeBonus.totalBonus -= employeeBonus.totalBonus % 1;
+  employeeBonus.totalCompensation += employeeBonus.totalBonus;
+  // if (employee.reviewRating === 3) {
+  //   bonusObj.bonusPercentage = 4;
+  // }
+  // if (employee.reviewRating === 4) {
+  //   bonusObj.bonusPercentage = 6;
+  // }
+  // if (employee.reviewRating === 5) {
+  //   bonusObj.bonusPercentage = 10;
+  // }
   // return new object with bonus results
   return employeeBonus;
 }
 for (let i = 0; i < employees.length; i++) {
-  console.log('employee data: ', calculateIndividualEmployeeBonus(employees[i]));
+  console.log('employee info:', employees[i]);
+  const employeeInfo = calculateIndividualEmployeeBonus(employees[i]);
+  console.log('employee bonus Info', employeeInfo);
 }
